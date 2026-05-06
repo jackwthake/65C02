4a5de4b79354d3ee855fa3bf2b5165a25ad210e7
jackwthake <jackthake@hotmail.com>
2026-05-06 03:23:09 -0700

    Now running C!!!

--- Makefile
+++ Makefile
@@ -1,24 +1,13 @@
-CC=cc65
-CA=ca65
-LD=ld65
+CC=~/llvm-mos/bin/mos-common-clang
 
-CFG=--target none
-
-default: bin bin/main.o bin/start.o
-	$(LD) -C cc65.cfg bin/*.o /usr/share/cc65/lib/none.lib -o bin/prog.bin
-
-bin/main.o: src/main.c
-	$(CC) $(CFG) src/main.c -o bin/main.s
-	$(CA) bin/main.s -o bin/main.o
-
-bin/start.o: src/start.s
-	$(CA) src/start.s -o bin/start.o
+default: bin
+	$(CC) -Os -T src/link.ld -o bin/prog.bin src/main.c
 
 bin:
 	mkdir -p bin
 
 upload: default
-	./minipro/minipro -p AT28C256 -u -w bin/prog.bin -s
+	./minipro/minipro -p AT28C256 -u -w bin/prog.bin
 
 clean:
 	rm -rf bin/*
--- src/main.c
+++ src/main.c
@@ -3,30 +3,31 @@
 #define DDRB  (*(volatile unsigned char*)0x6002)
 #define DDRA  (*(volatile unsigned char*)0x6003)
 
-const unsigned char E = 0b10000000;
-const unsigned char RW = 0b01000000;
-const unsigned char RS = 0b00100000;
+#define E 0b10000000
+#define RW 0b01000000
+#define RS 0b00100000
 
-void delay() {
-  volatile unsigned int i;
-  for (i = 0; i < 60000; ++i) {
-    asm("NOP");
-  }
+#define NOP5000() do { \
+  asm("ldx #$FF\n1:\ndex\nbne 1b\n" : : : "x"); \
+} while(0)
+
+void init_display(void);
+void print_str(const char *str);
+
+void __attribute__((interrupt)) irq(void) {
+  *(volatile unsigned char*)0x600D = 0x20;
 }
 
 void send_command(unsigned char cmd) {
   PORTB = cmd;
-  delay();
 
   PORTA = 0;
-  delay();
   PORTA = E;
-  delay();
+  NOP5000();
   PORTA = 0;
-  delay();
 }
 
-void print_str(char *str) {
+void print_str(const char *str) {
   char c;
   unsigned i = 0;
   
@@ -38,9 +39,8 @@ void print_str(char *str) {
     PORTB = c;
 
     PORTA = RS;
-    delay();
     PORTA = RS | E;
-    delay();
+    NOP5000();
     PORTA = RS;
 
     ++i;
@@ -53,17 +53,21 @@ void init_display() {
   DDRA = 0b11100000;
 
   send_command(0b00111000); // Set 8-bit mode; 2-line display; 5x8 font
-  send_command(0b00001110); // Display on; cursor on; blink off
+  send_command(0b00001111); // Display on; cursor on; blink on
   send_command(0b00000110); // Increment and shift cursor; don't shift display
 }
 
 int main(void) {
   init_display();
 
-  print_str("Hello, World!");
+  print_str("Hello, World!!");
 
-  while(1) {}
-  return 0;
+  unsigned char pattern = 0b10101010;
+  while(1) {
+    unsigned long int i;
+    PORTB = pattern;
+    pattern = (pattern << 1) | (pattern >> 7);
+    
+    for (i = 0; i < 12000; ++i) { asm("NOP"); }
+  }
 }
-
-// issue with stack pointer or somehting 
